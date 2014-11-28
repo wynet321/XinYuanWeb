f0e1014262e8a3cd197db876c4af446a3d46a6a1
wynet321 <wynet321@163.com>
2014-11-28 15:58:49 +0800

    homepage structure is done.

--- XinYuanWeb/js/script.js
+++ XinYuanWeb/js/script.js
@@ -2,24 +2,14 @@
  * dennis(wynet321@163.com)
  */
 
-function picShow(m) {
-	document.getElementById("pic_content_1").style.display = "none";
-	document.getElementById("pic_content_2").style.display = "none";
-	document.getElementById("pic_content_3").style.display = "none";
-	document.getElementById("pic_content_" + m).style.display = "block";
-	document.getElementById("pic_mini_1").className = "";
-	document.getElementById("pic_mini_2").className = "";
-	document.getElementById("pic_mini_3").className = "";
-	document.getElementById("pic_mini_" + m).className = "selected";
-}
-
 function sliderShow() {
 	if (!document.getElementById && !document.getElementsByTagName)
 		return false;
 	var slide = document.getElementById("slider");
 	var slideli = slide.getElementsByTagName("li");
+	var picwidth=2000;
 	for (var i = 0; i < slideli.length; i++) {
-		slideli[i].style.left += i * 940 + "px";
+		slideli[i].style.left += i * picwidth + "px";
 	}
 	var prev = document.getElementById("prev"), next = document
 			.getElementById("next");
@@ -29,14 +19,14 @@ function sliderShow() {
 	}
 	var xpos = parseInt(elem.style.marginLeft);
 	next.onclick = function() {
-		if (xpos > 0 || xpos <= -1880)
+		if (xpos > 0 || xpos <= -(picwidth*2))
 			return false;
-		moveElement("slider", xpos -= 940, 20);
+		moveElement("slider", xpos -= picwidth, 20);
 	}
 	prev.onclick = function() {
-		if (xpos >= 0 || xpos < -18800)
+		if (xpos >= 0 || xpos < -(picwidth*2))
 			return false;
-		moveElement("slider", xpos += 940, 20);
+		moveElement("slider", xpos += picwidth, 20);
 	}
 }
 function moveElement(elementID, gap, interval) {
